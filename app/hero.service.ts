8c39a93b3e055e75a5b4d7aebbe74283079c2af3
Pablo Ledesma <pabloledes83@gmail.com>
2016-07-12 22:47:05 -0500

    Add/Delete in the HeroesComponent

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { HEROES } from './mock-heroes';
@@ -34,6 +34,51 @@ export class HeroService {
 	private handleError(error: any) {
 	  console.error('An error occurred', error);
 	  return Promise.reject(error.message || error);
+	}
+
+	// Add new Hero
+	private post(hero: Hero): Promise<Hero> {
+	  let headers = new Headers({
+	    'Content-Type': 'application/json'});
+
+	  return this.http
+	             .post(this.heroesUrl, JSON.stringify(hero), {headers: headers})
+	             .toPromise()
+	             .then(res => res.json().data)
+	             .catch(this.handleError);
+	}
+
+	// Update existing Hero
+	private put(hero: Hero) {
+	  let headers = new Headers();
+	  headers.append('Content-Type', 'application/json');
+
+	  let url = `${this.heroesUrl}/${hero.id}`;
+
+	  return this.http
+	             .put(url, JSON.stringify(hero), {headers: headers})
+	             .toPromise()
+	             .then(() => hero)
+	             .catch(this.handleError);
+	}
+
+	delete(hero: Hero) {
+	  let headers = new Headers();
+	  headers.append('Content-Type', 'application/json');
+
+	  let url = `${this.heroesUrl}/${hero.id}`;
+
+	  return this.http
+	             .delete(url, headers)
+	             .toPromise()
+	             .catch(this.handleError);
+	}
+
+	save(hero: Hero): Promise<Hero>  {
+	  if (hero.id) {
+	    return this.put(hero);
+	  }
+	  return this.post(hero);
 	}
 
 }
